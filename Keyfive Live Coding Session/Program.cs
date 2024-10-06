dee767a2e9aa5df545677f499bfd18777adf334a
John S <john@keyfive.com>
2024-10-05 18:09:24 -0600

    Update wording

--- Keyfive Live Coding Session/Program.cs
+++ Keyfive Live Coding Session/Program.cs
@@ -1,23 +1,23 @@
-﻿// Keyfive Coding Test
+﻿//Keyfive Coding Test
 
-// Requirements
+//Requirements
 
-// Create a Telemetry.cs class file that Implements 'ITelemetry'
+//Update the Telemetry.cs class file which should Implement 'ITelemetry'
 
-// The Telemetry class should represent data collected from a device. Each telemetry object will have:
-//        DeviceId: Identifier for the device.
-//        Name: Name of the device.
-//        Timestamp: The time when the telemetry data was recorded.
-//        Value: The value of the data collected(e.g., temperature).
-//        Unit: The unit of the collected value(e.g., Celsius, Fahrenheit).
-// The 'UpdateValue' method should update the Value and refresh the Timestamp to the current time.
-// The 'Display()' method should print the telemetry data to the console.
+//The Telemetry class (or record) should represent data collected from a device. Each telemetry object will have:
+//       DeviceId: Identifier for the device.
+//       Name: Name of the device.
+//       Timestamp: The time when the telemetry data was recorded.
+//Value: The value of the data collected(e.g., temperature).
+//       Unit: The unit of the collected value(e.g., Celsius, Fahrenheit).
+//The 'UpdateValue' method should update the Value and refresh the Timestamp to the current time.
+//The 'Display()' method should print the telemetry data to the console.
 
-// The Unit Tests in the TelemetryTests.cs file will need to run and pass before the live coding session
-// Make sure you have restored the Xunit packages linked to the project. The following Nuget package will need to be restored/installed:
-//        Microsoft.NET.Test.Sdk
-//        xunit
-//        xunit.runner.visualstudio
+//The Unit Tests in the TelemetryTests.cs file will need to run and pass before the live coding session
+//Make sure you have restored the Xunit packages linked to the project.The following Nuget package will need to be restored / installed:
+//       Microsoft.NET.Test.Sdk
+//       xunit
+//       xunit.runner.visualstudio
 
 
 
