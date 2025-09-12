d96326a3b5866a3fe23f883b632b519bd1ceee58
Evaristo <evaristossfilho@hotmail.com>
2025-09-12 15:03:17 -0300

    Initial commit: Fixed dependencies.

--- Keyfive Live Coding Session/Telemetry.cs
+++ Keyfive Live Coding Session/Telemetry.cs
@@ -1,8 +1,31 @@
-﻿namespace KeyfiveLiveCodingSession;
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace KeyfiveLiveCodingSession;
+using static KeyfiveLiveCodingSession.ITelemetry;
 
 // Implement the Telemetry class using the ITelemetry interface. For bonus points, use a record instead of a class.
 
-public class Telemetry : ITelemetry
+public record Telemetry : ITelemetry
 {
+    public Telemetry(Guid deviceID, string name, double value, Units unit ) { }
+    public Guid DeviceId => throw new NotImplementedException();
 
+    public string Name => throw new NotImplementedException();
+
+    public DateTime Timestamp => throw new NotImplementedException();
+
+    public double Value => throw new NotImplementedException();
+
+    public ITelemetry.Units Unit => throw new NotImplementedException();
+
+    public void Display()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateValue(double newValue)
+    {
+        throw new NotImplementedException();
+    }
 }
